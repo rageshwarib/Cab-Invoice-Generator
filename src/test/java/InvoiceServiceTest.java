6983e19b2d57f48f9ab9a6ff67d3dae810d07d40
rageshwari <ragu.bodke@gmail.com>
2020-04-16 20:39:28 +0530

    ADDED:Code that given user id, Invoice Service gets the List of rides and returs Invoice

--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -1,33 +1,43 @@
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
-    InvoiceGenerator invoiceGenerator = null;
+    InvoiceService invoiceService = null;
     @Test
     public void givenDistanceAndTime_shouldReturnTotalFare(){
-        invoiceGenerator = new InvoiceGenerator();
+        invoiceService = new InvoiceService();
         double distance = 2.0;
         int time = 5;
-        double fare = invoiceGenerator.calculateFare(distance, time);
+        double fare = invoiceService.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
     }
     @Test
     public void givenLessDistanceAndTime_shouldReturnTotalFare() {
-        invoiceGenerator = new InvoiceGenerator();
+        invoiceService = new InvoiceService();
         double distance = 0.1;
         int time = 1;
-        double fare = invoiceGenerator.calculateFare(distance, time);
+        double fare = invoiceService.calculateFare(distance, time);
         Assert.assertEquals(5, fare, 0.0);
     }
     @Test
     public void givenMultipleRides_shouldReturnInvoiceSummary() {
-        invoiceGenerator = new InvoiceGenerator();
+        invoiceService = new InvoiceService();
         Ride[] rides = { new Ride(2.0, 5),
                 new Ride(0.1, 1),
         };
-        InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
+        InvoiceSummary summary = invoiceService.calculateFare(rides);
         InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
         Assert.assertEquals(expectedInvoiceSummary, summary);
     }
+    @Test
+    public void givenUserIdAndRides_shouldReturnInvoiceSummary() {
+        invoiceService = new InvoiceService();
+        String userId = "abc.com";
+        Ride[] rides = { new Ride(2.0, 5),
+                new Ride(0.1, 1),
+        };
+        invoiceService.addRides(userId , rides);
+        InvoiceSummary summary = invoiceService.getInvoiceSummary(userId);
+    }
+
 }
